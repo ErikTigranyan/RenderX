303715fd06f603bd78c687969b00e9d253666979
ErikTigranyan <eriktigranyan90@gmail.com>
2025-03-22 02:35:10 +0400

    Added fixed function pipeline stages configs

--- RenderX/src/Pipeline.h
+++ RenderX/src/Pipeline.h
@@ -7,11 +7,24 @@
 #include <vector>
 
 namespace rex {
-	struct PipelineConfigInfo {};
+	struct PipelineConfigInfo {
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
+	};
 	class Pipeline {
 	public:
 		Pipeline(Device& device, const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo& configInfo);
-		~Pipeline() {};
+		~Pipeline();
 
 		Pipeline(const Pipeline&) = delete;
 		void operator=(const Pipeline&) = delete;
