702ac46e209bc08fc27ccb1eac1009cfd671b29d
ErikTigranyan <eriktigranyan90@gmail.com>
2025-04-08 18:41:59 +0400

    Updated the vertex struct and added color attribute

--- RenderX/src/Application.cpp
+++ RenderX/src/Application.cpp
@@ -27,9 +27,9 @@ namespace rex {
 
 	void Application::loadModels() {
 		std::vector<Model::Vertex> vertices{
-			{{0.0f, -0.5f}},
-			{{0.5f, 0.5f}},
-			{{-0.5f, 0.5f}}
+			{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+			{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+			{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
 		};
 
 		model = std::make_unique<Model>(device, vertices);
