d1437a9eda936d0086dfeb023ed1b3b547789d1a
ErikTigranyan <eriktigranyan90@gmail.com>
2025-05-30 01:27:30 +0400

    Added file wavefront obj loader, to load 3D models into the engine

--- RenderX/src/Application.cpp
+++ RenderX/src/Application.cpp
@@ -50,77 +50,15 @@ namespace rex {
 				renderer.endFrame();
 			}
 		}
-
 		vkDeviceWaitIdle(device.device());
 	}
 
-    // temporary helper function, creates a 1x1x1 cube centered at offset with an index buffet
-    std::unique_ptr<Model> createCubeModel(Device& device, glm::vec3 offset) {
-        Model::Builder modelBuilder{};
-        modelBuilder.vertices = {
-
-            // left face (white)
-            {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-            {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-            {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-            {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-            {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
-            {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-
-            // right face (yellow)
-            {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-            {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-            {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-            {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-
-            // top face (orange, remember y axis points down)
-            {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-            {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-            {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-            {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-            {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-            {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-
-            // bottom face (red)
-            {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-            {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-            {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-            {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-            {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-
-            // nose face (blue)
-            {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-            {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-            {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-            {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-            {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-            {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-
-            // tail face (green)
-            {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-            {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-            {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-            {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-            {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-            {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-
-        };
-        for (auto& v : modelBuilder.vertices) {
-            v.position += offset;
-        }
-        return std::make_unique<Model>(device, modelBuilder);
-    }
-
-	void Application::loadGameObjects() {
-        std::shared_ptr<Model> model = createCubeModel(device, { .0f, .0f, .0f });
-
-        auto cube = GameObject::createGameObject();
-        cube.model = model;
-        cube.transform.translation = {.0f, .0f, 2.5f};
-        cube.transform.scale = { .5f, .5f, .5f };
-        gameObjects.push_back(std::move(cube));
+ 	void Application::loadGameObjects() {
+        std::shared_ptr<Model> model = Model::createModelFromFile(device, "../../RenderX/models/cube.obj");
+        auto gameObj = GameObject::createGameObject();
+        gameObj.model = model;
+        gameObj.transform.translation = { .0f, .0f, 2.5f };
+        gameObj.transform.scale = glm::vec3(3.f);
+        gameObjects.push_back(std::move(gameObj));
 	}
 } // namespace rex
--- RenderX/src/Model.cpp
+++ RenderX/src/Model.cpp
@@ -1,11 +1,33 @@
 #include "Model.h"
+#include "Utils.h"
 
+// libs
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
+
+// std
 #include <cassert>
+#include <cstring>
+#include <iostream>
+
+namespace std {
+	template <>
+	struct hash<rex::Model::Vertex> {
+		size_t operator()(rex::Model::Vertex const &vertex) const {
+			size_t seed = 0;
+			rex::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+			return seed;
+		}
+	};
+}  // namespace std
 
 namespace rex {
-	Model::Model(Device& modelDevice, const Model::Builder &builder) : device{modelDevice} {
+	Model::Model(Device &modelDevice, const Model::Builder &builder) : device{modelDevice} {
 		createVertexBuffers(builder.vertices);
-		createIndexBuffers(builder.indicies);
+		createIndexBuffers(builder.indices);
 	};
 
 	Model::~Model() {
@@ -17,6 +39,12 @@ namespace rex {
 			vkFreeMemory(device.device(), indexBufferMemory, nullptr);
 		}
 	};
+
+	std::unique_ptr<Model> Model::createModelFromFile(Device& device, const std::string& filepath) {
+		Builder builder{};
+		builder.loadModel(filepath);
+		return std::make_unique<Model>(device, builder);
+	}
 
 	void Model::createVertexBuffers(const std::vector<Vertex>& vertices) {
 		vertexCount = static_cast<uint32_t>(vertices.size());
@@ -40,15 +68,15 @@ namespace rex {
 		vkFreeMemory(device.device(), stagingBufferMemory, nullptr);
 	}
 
-	void Model::createIndexBuffers(const std::vector<uint32_t> &indicies) {
-		indexCount = static_cast<uint32_t>(indicies.size());
+	void Model::createIndexBuffers(const std::vector<uint32_t> &indices) {
+		indexCount = static_cast<uint32_t>(indices.size());
 		hasIndexBuffer = indexCount > 0;
 
 		if (!hasIndexBuffer) {
 			return;
 		}
 
-		VkDeviceSize bufferSize = sizeof(indicies[0]) * indexCount;
+		VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
 
 		VkBuffer stagingBuffer;
 		VkDeviceMemory stagingBufferMemory;
@@ -57,7 +85,7 @@ namespace rex {
 
 		void* data;
 		vkMapMemory(device.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
-		memcpy(data, indicies.data(), static_cast<size_t>(bufferSize));
+		memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
 		vkUnmapMemory(device.device(), stagingBufferMemory);
 
 		device.createBuffer(bufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
@@ -105,5 +133,67 @@ namespace rex {
 		attributeDescriptions[1].offset = offsetof(Vertex, color);
 
 		return attributeDescriptions;
+	}
+
+	void Model::Builder::loadModel(const std::string &filepath) {
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warn, err;
+
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+			throw std::runtime_error(warn + err);
+		}
+
+		vertices.clear();
+		indices.clear();
+
+		std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+		for (const auto& shape : shapes) {
+			for (const auto& index : shape.mesh.indices) {
+				Vertex vertex{};
+
+				if (index.vertex_index >= 0) {
+					vertex.position = {
+						attrib.vertices[3 * index.vertex_index + 0],
+						attrib.vertices[3 * index.vertex_index + 1],
+						attrib.vertices[3 * index.vertex_index + 2],
+					};
+
+					auto colorIndex = 3 * index.vertex_index + 2;
+					if (colorIndex < attrib.colors.size()) {
+						vertex.color = {
+							attrib.colors[colorIndex - 2],
+							attrib.colors[colorIndex - 1],
+							attrib.colors[colorIndex - 0],
+						};
+					}
+					else {
+						vertex.color = { 1.f, 1.f, 1.f };  // set default color
+					}
+				}
+
+				if (index.normal_index >= 0) {
+					vertex.normal = {
+						attrib.normals[3 * index.normal_index + 0],
+						attrib.normals[3 * index.normal_index + 1],
+						attrib.normals[3 * index.normal_index + 2],
+					};
+				}
+
+				if (index.texcoord_index >= 0) {
+					vertex.uv = {
+						attrib.texcoords[2 * index.texcoord_index + 0],
+						attrib.texcoords[2 * index.texcoord_index + 1],
+					};
+				}
+
+				if (uniqueVertices.count(vertex) == 0) {
+					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertex);
+				}
+				indices.push_back(uniqueVertices[vertex]);
+			}
+		}
 	}
 } // namespace rex
